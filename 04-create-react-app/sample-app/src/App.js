57bcdf109980b773c3717e6eb62d9558ae0be5c2
unknown <ywarezk@gmail.com>
2020-02-02 16:38:57 +0200

    partial create react app

---
+++ 04-create-react-app/sample-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import {Login} from "@cp/auth";
+// import Login from "@cp/auth/login"
+import Header from "./components/common/Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ 04-create-react-app/sample-app/src/components/common/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.scss";
+
+export default function Header() {
+    return (
+        <header className="cp-header">
+            <h1>
+                hello im the header
+            </h1>
+        </header>
+    )
+}
